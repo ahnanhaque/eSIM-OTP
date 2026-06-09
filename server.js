9bc2b8e9fb00993ab41c0dbd0f5cd0a4030b5746
Ahnan Haque <282889889+ahnanhaque@users.noreply.github.com>
2026-06-09 16:07:57 +0600

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,8 @@ const zenex          = require("./zenex.js");
 const nxa            = require("./nxa.js");
 const { detectCountryFromRange, getCountryInfo } = require("./country.js");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI(
-    process.env.GEMINI_API_KEY
-);
-
 const geminiModel = genAI.getGenerativeModel({
-    model: "gemini-2.5-flash"
+    model: "gemini-2.0-flash"
 });
 const admin = require("firebase-admin");
 const serviceAccount = require("./firebase-service.json");
